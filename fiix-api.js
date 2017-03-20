61ac436e093933f95d959d9c3eac3e02ae7cad83
nsbabrah <navjotbabrah27@gmail.com>
2017-03-20 13:25:09 -0400

    Update fiix-api.js

--- fiix-api.js
+++ fiix-api.js
@@ -1,9 +1,9 @@
 
 var fiixCmmsClient = new fiixCmmsClient();
 fiixCmmsClient.setBaseUri( 'https://raspberry.masandbox.com/api/' );
-fiixCmmsClient.setAppKey('macmmsackp384040a2a90e5302d927a02377c0c25ff09be64d42531d834d688');
-fiixCmmsClient.setAuthToken('macmmsaakp384dcdab6d977264ceb04644784c3eefe111c79358d34d94d860b3e');
-fiixCmmsClient.setPKey('macmmsaskp384f1b2808e6d8d94b23017e5e55d0753bb9c24e83b42cbe0f0c21468aa60f8fb0f');
+fiixCmmsClient.setAppKey('');
+fiixCmmsClient.setAuthToken('');
+fiixCmmsClient.setPKey('');
 
 
 
